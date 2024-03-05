0c55c4e80b603da7a92361dffe82f67426a4ab24
Alan <a8576954111@gmail.com>
2024-03-05 23:51:04 +0800

    background

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -32,7 +32,9 @@ void CGameStateRun::OnMove()							// 移動遊戲元素
 
 void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 {
-	
+	background.LoadBitmapByString({
+		"Resources/initialize_background.bmp"});
+	background.SetTopLeft(0, 0);
 }
 
 void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
@@ -67,4 +69,16 @@ void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
 
 void CGameStateRun::OnShow()
 {
+	show_image_by_phase();
+	show_text_by_phase();
+}
+
+void CGameStateRun::show_image_by_phase() {
+	background.ShowBitmap();
+}
+void CGameStateRun::show_text_by_phase() {
+	CDC *pDC = CDDraw::GetBackCDC();
+
+	CTextDraw::ChangeFontLog(pDC, 21, "微軟正黑體", RGB(0, 0, 0), 800);
+
 }
